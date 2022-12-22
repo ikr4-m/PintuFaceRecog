6ca128a700174a1f247f8ae795c74f04b28a04f2
Ikramullah <ikramullahzx@gmail.com>
2022-12-22 20:16:27 +0800

    feat: tambah buzzer dan push button untuk escape

--- Engine/Engine.ino
+++ Engine/Engine.ino
@@ -1,8 +1,12 @@
 #include <Servo.h>
 
 // Konfigurasi Servo
-const uint8_t SERVO_PIN = D0;
+const uint8_t SERVO_PIN = D2;
 Servo servo;
+
+// Konfigurasi buzzer & push button
+const uint8_t BUZZER_PIN = D0;
+const uint8_t PUSH_BUTTON_PIN = D1;
 
 void animateServo(int count, long delayMs, bool is90Degree = true) {
   if (count < 1) return;
@@ -12,6 +16,21 @@ void animateServo(int count, long delayMs, bool is90Degree = true) {
     delay(delayMs);
     servo.write(0);
   }
+}
+
+void animateBuzzer(int count, long delayMs) {
+  if (count < 1) return;
+  for (int i = 0; i < count; i++) {
+    if (i != 0) delay(delayMs);
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+}
+
+void unlockDoor() {
+  animateBuzzer(1, 250);
+  animateServo(1, 5000);
 }
 
 void setup() {
@@ -26,15 +45,25 @@ void setup() {
   // Konfigurasi Servo
   servo.attach(SERVO_PIN, 500, 2400);
   servo.write(0);
+
+  // Konfigurasi buzzer dan tombol
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(PUSH_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
+  // Kalau tombol dipencet, ya dibuka bae
+  if (digitalRead(PUSH_BUTTON_PIN) == LOW) {
+    unlockDoor();
+    return;
+  }
+
   // Kalau bukan dari serial, skip
   if (Serial.available() == 0) return;
 
   // Panggil serialnya di sini
   switch (Serial.readString().toInt()) {
     case 0: animateServo(5, 500); break;
-    case 1: animateServo(1, 5000); break; 
+    case 1: unlockDoor(); break; 
   }
 }
