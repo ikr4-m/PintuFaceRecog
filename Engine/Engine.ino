0657d532c1972ef4da8a55705abf120b94faa048
Ikramullah <ikramullahzx@gmail.com>
2022-12-22 16:52:34 +0800

    test: tes koneksi kembali dari python ke arduino

--- Engine/Engine.ino
+++ Engine/Engine.ino
@@ -1,17 +1,39 @@
+#include <Servo.h>
+
+// Konfigurasi Servo
+const uint8_t SERVO_PIN = D0;
+Servo servo;
+
+bool getFromSerial(int data) {
+  return Serial.available() > 0 && Serial.readString().toInt() == data;
+}
+
+void animateServo(int count, long delayMs, bool is90Degree = true) {
+  if (count < 1) return;
+  for (int i = 0; i < count; i++) {
+    if (i != 0) delay(delayMs);
+    servo.write(0);
+    delay(delayMs);
+    servo.write(is90Degree ? 90 : 180);
+  }
+}
+
 void setup() {
+  // Buka serial pembacaan data ke komputer
   Serial.begin(9600);
   Serial.setTimeout(1);
+  Serial.println(F("Receiving handshake."));
 
+  // Konfigurasi LED
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+
+  // Konfigurasi Servo
+  servo.attach(SERVO_PIN, 500, 2400);
+  servo.write(90);
 }
 
 void loop() {
-  if (Serial.available() == 0) return;
-  bool x = Serial.readString().toInt() == 1;
-  if (x) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
+  // Handshake untuk arduino
+  if (getFromSerial(0)) animateServo(1, 500);
 }
--- FaceReg/main.py
+++ FaceReg/main.py
@@ -2,4 +2,4 @@ from decouple import config
 from serial import Serial
 
 arduino = Serial(port=config('ARDUINO_PORT_SERIAL'), baudrate=9600, timeout=.1)
-arduino.write(bytes("1", 'utf-8'))
+arduino.write(bytes("0", 'utf-8'))
